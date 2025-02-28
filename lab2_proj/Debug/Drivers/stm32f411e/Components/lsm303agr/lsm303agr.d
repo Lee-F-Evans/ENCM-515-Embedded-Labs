0ef647ea89771548e67671f4d7507f717e9795b3
LeeF <lee.evans@ucalgary.ca>
2025-02-27 23:16:32 -0700

    finished lab2

---
+++ lab2_proj/Debug/Drivers/stm32f411e/Components/lsm303agr/lsm303agr.d
@@ -0,0 +1,6 @@
+Drivers/stm32f411e/Components/lsm303agr/lsm303agr.o: \
+ ../Drivers/stm32f411e/Components/lsm303agr/lsm303agr.c \
+ ../Drivers/stm32f411e/Components/lsm303agr/lsm303agr.h \
+ C:/Users/LeeFE/Desktop/ENCM-515-Embedded-Labs/lab2_proj/Drivers/stm32f411e/Inc/../Common/accelero.h
+../Drivers/stm32f411e/Components/lsm303agr/lsm303agr.h:
+C:/Users/LeeFE/Desktop/ENCM-515-Embedded-Labs/lab2_proj/Drivers/stm32f411e/Inc/../Common/accelero.h:
